5f55b76d55609fc6df002566a5b326b86957f1e1
ShashankSagarJha <43700515+ShashankSagarJha@users.noreply.github.com>
2020-09-15 12:49:53 +0530

    Add files via upload

---
+++ Left Rotation.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    size=len(arr)
+    r_d=size-d #finding right rotation distance
+    new_list=[None] * size
+    for i in range(size):
+        new_list[(i+r_d)%size]=arr[i]
+    return new_list
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
